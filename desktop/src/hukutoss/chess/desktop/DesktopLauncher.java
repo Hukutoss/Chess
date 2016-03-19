f88fb04a3841a582a054c25d7b7d9fab3b43a395
Hukutoss <hukutoss9666@gmail.com>
2016-03-19 16:10:32 +0300

    Basic game stuff

---
+++ desktop/src/hukutoss/chess/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hukutoss.chess.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hukutoss.chess.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
