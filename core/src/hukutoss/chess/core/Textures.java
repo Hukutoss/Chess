18616fef37845798187d6d82f184727fb9e1321c
Hukutoss <hukutoss9666@gmail.com>
2016-03-30 23:25:18 +0300

    deleted useless thing

--- core/src/hukutoss/chess/core/Textures.java
+++ core/src/hukutoss/chess/core/Textures.java
@@ -17,12 +17,10 @@ public class Textures {
 
     private Textures() {}
 
-    private static Texture board_sheet;
+    private static Texture texture;
 
     public static Sprite white_cell;
     public static Sprite black_cell;
-
-    private static Texture pieces_sheet;
 
     public static Sprite white_king;
     public static Sprite white_queen;
@@ -42,25 +40,25 @@ public class Textures {
     private static final int cellSize = 64;
 
     public static void load() {
-        board_sheet = new Texture(Gdx.files.internal("textures/board.png"));
+        texture = new Texture(Gdx.files.internal("textures/board.png"));
 
-        white_cell = new Sprite(board_sheet, cellSize * 0, cellSize * 0, cellSize, cellSize);
-        black_cell  = new Sprite(board_sheet, cellSize * 1, cellSize * 0, cellSize, cellSize);
+        white_cell = new Sprite(texture, cellSize * 0, cellSize * 0, cellSize, cellSize);
+        black_cell  = new Sprite(texture, cellSize * 1, cellSize * 0, cellSize, cellSize);
 
-        pieces_sheet = new Texture(Gdx.files.internal("textures/pieces.png"));
+        texture = new Texture(Gdx.files.internal("textures/pieces.png"));
 
-        white_king = new Sprite(pieces_sheet, pieceSize * 0, pieceSize * 0, pieceSize, pieceSize);
-        white_queen = new Sprite(pieces_sheet, pieceSize * 1, pieceSize * 0, pieceSize, pieceSize);
-        white_bishop = new Sprite(pieces_sheet, pieceSize * 2, pieceSize * 0, pieceSize, pieceSize);
-        white_knight = new Sprite(pieces_sheet, pieceSize * 3, pieceSize * 0, pieceSize, pieceSize);
-        white_rook = new Sprite(pieces_sheet, pieceSize * 4, pieceSize * 0, pieceSize, pieceSize);
-        white_pawn = new Sprite(pieces_sheet, pieceSize * 5, pieceSize * 0, pieceSize, pieceSize);
+        white_king = new Sprite(texture, pieceSize * 0, pieceSize * 0, pieceSize, pieceSize);
+        white_queen = new Sprite(texture, pieceSize * 1, pieceSize * 0, pieceSize, pieceSize);
+        white_bishop = new Sprite(texture, pieceSize * 2, pieceSize * 0, pieceSize, pieceSize);
+        white_knight = new Sprite(texture, pieceSize * 3, pieceSize * 0, pieceSize, pieceSize);
+        white_rook = new Sprite(texture, pieceSize * 4, pieceSize * 0, pieceSize, pieceSize);
+        white_pawn = new Sprite(texture, pieceSize * 5, pieceSize * 0, pieceSize, pieceSize);
 
-        black_king = new Sprite(pieces_sheet, pieceSize * 0, pieceSize * 1, pieceSize, pieceSize);
-        black_queen = new Sprite(pieces_sheet, pieceSize * 1, pieceSize * 1, pieceSize, pieceSize);
-        black_bishop = new Sprite(pieces_sheet, pieceSize * 2, pieceSize * 1, pieceSize, pieceSize);
-        black_knight = new Sprite(pieces_sheet, pieceSize * 3, pieceSize * 1, pieceSize, pieceSize);
-        black_rook = new Sprite(pieces_sheet, pieceSize * 4, pieceSize * 1, pieceSize, pieceSize);
-        black_pawn = new Sprite(pieces_sheet, pieceSize * 5, pieceSize * 1, pieceSize, pieceSize);
+        black_king = new Sprite(texture, pieceSize * 0, pieceSize * 1, pieceSize, pieceSize);
+        black_queen = new Sprite(texture, pieceSize * 1, pieceSize * 1, pieceSize, pieceSize);
+        black_bishop = new Sprite(texture, pieceSize * 2, pieceSize * 1, pieceSize, pieceSize);
+        black_knight = new Sprite(texture, pieceSize * 3, pieceSize * 1, pieceSize, pieceSize);
+        black_rook = new Sprite(texture, pieceSize * 4, pieceSize * 1, pieceSize, pieceSize);
+        black_pawn = new Sprite(texture, pieceSize * 5, pieceSize * 1, pieceSize, pieceSize);
     }
 }
